95ee0d51724f5b054c7feee3baa6dc5777ea7b24
Davranbek <davranbek_1995@mail.ru>
2023-08-28 00:21:18 +0500

    added project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,43 @@
+
+import { useState, useEffect } from "react";
+import "./style.scss";
+
+const baseURL = "http://worldtimeapi.org/api/ip";
+import axios from "axios";
+const index = () => {
+
+    const [time, setTime] = useState("");
+    async function getTime(){
+        try{
+            const response = await axios.get(`${baseURL}`);
+            setTime(response.data);
+            console.log(response.data)
+        }catch(error){
+            console.log(error.message);
+        }
+       
+    }
+    useEffect(() => {
+        getTime();
+    }, []);
+
+    const weekName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const datetime = new Date(time.datetime);
+    return (
+        <header>
+            <div className="container">
+                <div className="flex items-center justify-between h-[80px] bg-slate-100 px-5">
+                    <h2 className="font-bold text-2xl">World Time Zone</h2>
+                    <div className="text-lg font-semibold">
+                        <p>Location: <span className="font-normal">{time.timezone}</span></p>
+                        <p>Time: <span className="font-normal"> {datetime.getHours()}:{datetime.getMinutes()}.{datetime.getSeconds()}</span></p> 
+                        {/* <p>Date: <span> {datetime.getDate()}/{datetime.getMonth() + 1}/{datetime.getFullYear()}</span></p>
+                        <p>Week: <span>{weekName[time.day_of_week]}</span></p> */}
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default index;
